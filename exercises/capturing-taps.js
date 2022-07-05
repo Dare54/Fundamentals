8128ea7699cc3681ddc7a417f64d5cabe91342c7
Dare <edidiongekrikpo94@gmail.com>
2022-07-05 09:15:58 +0100

    react-native-fundamentals

--- exercises/capturing-taps.js
+++ exercises/capturing-taps.js
@@ -1,9 +1,22 @@
-// Capturing User Taps
-
-// TIP: Button is a component exposed by React Native. You'll import it the same way you did View
-// and Text previously.
+import React from "react";
+import { View, Button, TouchableOpacity, Text } from "react-native";
 
 export default () => {
-  // Replace return null with your code.
-  return null;
+  return (
+    <View>
+      <Button title="Button 1" onPress={() => alert("hello!")} />
+      <TouchableOpacity
+        onPress={() => alert("hello 2!")}
+        style={{
+          backgroundColor: "brown",
+          paddingHorizontal: 10,
+          paddingVertical: 5,
+          alignSelf: "center",
+          marginTop: 10,
+        }}
+      >
+        <Text style={{ color: "white", fontSize: 18 }}>Button 2</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
