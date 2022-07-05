a67964da3a7bbf376bf9a99b7e498b39aa8c5a67
Dare <edidiongekrikpo94@gmail.com>
2022-07-05 09:31:31 +0100

    react-native-fundamentals

--- exercises/component-lifecycle.js
+++ exercises/component-lifecycle.js
@@ -1,10 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 
-import { listener, listener2 } from "../support/component-lifecycle";
+import { listener } from "../support/component-lifecycle";
 
 export default () => {
   const [updateCount, setUpdateCount] = useState(0);
+
+  useEffect(() => {
+    setUpdateCount(0);
+    const unsubscibe = listener(() => {
+      setUpdateCount((value) => value + 1);
+    });
+
+    return () => {
+      unsubscibe();
+    };
+  }, []);
 
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
--- exercises/custom-component.js
+++ exercises/custom-component.js
@@ -1,6 +1,24 @@
-// Creating a Custom Component
+import React from "react";
+import { TouchableOpacity, Text, View } from "react-native";
 
-export default () => {
-  // return <Button text="Hello" onPress={() => alert('hello!')} />
-  return null;
-};
+const Button = ({ text, onPress }) => (
+  <TouchableOpacity
+    onPress={onPress}
+    style={{
+      backgroundColor: "#ff637c",
+      alignSelf: "center",
+      paddingHorizontal: 20,
+      paddingVertical: 10,
+      marginVertical: 15,
+    }}
+  >
+    <Text style={{ color: "#fff" }}>{text}</Text>
+  </TouchableOpacity>
+);
+
+export default () => (
+  <View>
+    <Button text="Say hello" onPress={() => alert("hello!")} />
+    <Button text="Say goodbye" onPress={() => alert("goodbye!")} />
+  </View>
+);
