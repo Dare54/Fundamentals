97f07bb17c4e1dbaa27b389ecf34807dc2f2e1af
Dare <edidiongekrikpo94@gmail.com>
2022-07-05 10:43:49 +0100

    react-native-fundamentals

--- exercises/multiple-files.js
+++ exercises/multiple-files.js
@@ -1,9 +1,18 @@
 import React from "react";
-import { View } from "react-native";
-import { Button } from "./multiple-files.component";
+import { TouchableOpacity, Text } from "react-native";
 
-export default () => (
-  <View style={{ marginTop: 20 }}>
-    <Button text="Say hello" onPress={() => alert("hello!")} />
-  </View>
+export const Button = ({ text, onPress, buttonStyle = {} }) => (
+  <TouchableOpacity
+    onPress={onPress}
+    style={{
+      backgroundColor: "#ff637c",
+      alignSelf: "center",
+      paddingHorizontal: 20,
+      paddingVertical: 10,
+      marginVertical: 15,
+      ...buttonStyle,
+    }}
+  >
+    <Text style={{ color: "#fff" }}>{text}</Text>
+  </TouchableOpacity>
 );
